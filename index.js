676376919829043e083cc80078428d438ebd49c8
ThinhNguyen <thinhproee@gmail.com>
2024-09-07 22:12:21 +0700

    cấu hình view

--- index.js
+++ index.js
@@ -2,15 +2,22 @@ const express = require('express');//  imports the Express module
 const app = express();// initialize an Express application instance.
 const port = 3000;
 
+//Start PUG
+// Set the view(template) engine to Pug
+app.set('view engine', 'pug');
+
+// Set the directory where the Pug templates are located
+app.set('views', './views');
+//End PUG
 
 
 app.get('/', (req, res) => {
-    res.send('Trang chủ');
+    res.render('client/pages/home/index');
 });
 
 
 app.get('/products', (req, res) => {
-    res.send('Trang danh sách sản phẩm');
+    res.render('client/pages/products/index');
 });
 
 // Start the server
