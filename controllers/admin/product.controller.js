07eaf65175b45cece0a4f700cd73c59ccf268b01
ThinhNguyen <thinhproee@gmail.com>
2024-09-23 13:36:24 +0700

    tim kiem admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -7,15 +7,28 @@ module.exports.viewProduct = async (req, res) => {
     // object in object
     //phần lọc filter xem thêm cách mình làm
 
+    ///// Filter
     const objectFind={
         deleted: false
     };
 
 
-    if (req.query.status) {
+    if (req.query.status) 
         objectFind.status = req.query.status;
-    }
     //console.log(objectFind);
+    //////////// End Filter
+
+
+    // search
+    if (req.query.keyword)
+    {
+        const regex = new RegExp(req.query.keyword, "i");//"i" flag: Case-insensitive search: Tìm kiếm không phân biệt chữ hoa chữ thường
+        objectFind.title = regex;
+    }
+    //end search
+
+
+
 
     const danhSachSanPham = await ProductModel.find(objectFind);
     //console.log('test xem có danh sách sản phẩm không: ',danhSachSanPham);
--- public/admin/scripts/main.js
+++ public/admin/scripts/main.js
@@ -21,13 +21,49 @@ if (boxFilter) {
         
         //console.log(url);
         //console.log(url.href);
+        // Redirect the page with the new filter
         location.href = url.href;
     })
 
+
+    // url có gì thì in ra cái field đó hiện ra để dễ xem
     // Hiển thị lựa chọn mặc định, khi lần đầu vào trang chưa làm gì cả
+    // Set the default value in the filter dropdown
     const statusCurrent = url.searchParams.get("status");
     if (statusCurrent) 
         boxFilter.value = statusCurrent;
     
 }
 // Hết Bộ lọc
+
+
+
+// Tìm kiếm
+// Search by keyword
+const formSearch = document.querySelector("[form-search]");
+if (formSearch) {
+  formSearch.addEventListener("submit", (event) => {
+    event.preventDefault(); // Prevent form submission
+
+    let url = new URL(location.href);
+    const keyword = formSearch.keyword.value.trim();
+
+    if (keyword) {
+      // If there is a keyword, add/update it in the URL parameters
+      url.searchParams.set("keyword", keyword);
+    } else {
+      // If the keyword is empty, remove the 'keyword' parameter from the URL
+      url.searchParams.delete("keyword");
+    }
+
+    // Redirect the page with the new or updated parameters
+    location.href = url.href;
+  });
+
+  // Set the default search keyword in the input field if it exists in the URL
+  const valueCurrent = new URL(location.href).searchParams.get("keyword");
+  if (valueCurrent) {
+    formSearch.keyword.value = valueCurrent;
+  }
+}
+// Hết Tìm kiếm
